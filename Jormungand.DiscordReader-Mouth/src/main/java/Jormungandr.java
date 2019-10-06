65fd230691418644993dee6b502dd15118dcd864
MgBacon <mgcbkmn@googlemail.com>
2019-10-06 20:45:05 +0200

    First (broken) draft of JSON-ifying JDA events

--- Jormungand.DiscordReader-Mouth/src/main/java/Jormungandr.java
+++ Jormungand.DiscordReader-Mouth/src/main/java/Jormungandr.java
@@ -1,19 +1,21 @@
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class Jormungandr extends ListenerAdapter {
 
-    public static void main(String[] args) throws LoginException, InterruptedException
-    {
+    public static void main(String[] args) throws LoginException, InterruptedException {
         try (Stream<String> stream=Files.lines(Paths.get("config.txt"))) {
             String token=stream.findFirst().get();
             JDA  jda = new JDABuilder(token)
@@ -25,9 +27,12 @@ public class Jormungandr extends ListenerAdapter {
             e.printStackTrace();
         }
     }
+    private Gson gson=new Gson();
+    private Type token = new TypeToken<GenericEvent>(){}.getType();
     @Override
-    public void onGenericEvent(GenericEvent event)
-    {
+    public void onGenericEvent(GenericEvent event) {
+        String eventData=gson.toJson(event, token);
         System.out.println(event.getClass().getName());
+        System.out.println(eventData);
     }
 }
